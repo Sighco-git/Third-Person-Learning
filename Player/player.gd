8d1815e87cccd9f83e5f90dfaa382c47c865ac80
Sighco <dxst482wts@privaterelay.appleid.com>
2026-06-09 11:16:52 -0500

    Update

--- Player/player.gd
+++ Player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+@onready var placeholder_mesh: MeshInstance3D = $PlaceholderMesh
 @onready var spring_arm_3d: SpringArm3D = $Pivot/SpringArm3D
 @onready var pivot: Node3D = $Pivot
 @export var sensitivity := 0.005
@@ -30,7 +31,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_backward")
 	var direction = (pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -38,5 +39,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+	placeholder_mesh.rotation.y = input_dir.angle()
 	move_and_slide()
--- Weapons/Systems/dynamic_crosshair.gd
+++ Weapons/Systems/dynamic_crosshair.gd
@@ -3,12 +3,14 @@ extends Sprite3D
 @onready var muzzle_raycast: RayCast3D = $".."
 @export var weight := 1.0
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if muzzle_raycast.is_colliding():
+func _physics_process(delta: float) -> void:
+	if muzzle_raycast.is_colliding() && Input.is_action_just_pressed("attack"):
 		var predicted_shot = muzzle_raycast.get_collision_point()
-		position = lerp(position, predicted_shot, weight) * delta
+		global_position = lerp(global_position, predicted_shot, weight) * delta
+		print(predicted_shot)
